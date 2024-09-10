fd18800e0998226ac84e8685533bf152b879e369
Benoit Razet <benoit.razet@galois.com>
2024-09-10 16:30:24 -0400

    Add tutorial on tactics

---
+++ CharlestonLeanMeetup/2024-09/tactics.lean
@@ -0,0 +1,495 @@
+-------------
+-- Tactics --
+-------------
+
+-- Lean also allows to prove using "tactic-style" proofs, as opposed to "term-style proofs"
+-- Tactics is a gateway to automation since automation is often implemented as tactics.
+
+theorem test (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  sorry
+
+theorem test2 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p :=
+  -- by tactics
+  -- NOTE: the following is very sensitive to spacing
+  by apply And.intro
+     exact hp
+     apply And.intro
+     exact hq
+     exact hp
+
+#print test2
+
+-- In VS-code CTRL-SHIFT-ENTER to see the message window
+
+-- what is interesting here is that only one subgoal
+theorem test3 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro hp
+  exact And.intro hq hp
+
+-- use semicolon `;` to sequence tactics
+theorem test4 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro hp; exact And.intro hq hp
+
+-- Use case to name specific branch to work on
+theorem test5 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  case right =>
+    apply And.intro
+    case left => exact hq
+    case right => exact hp
+  case left => exact hp
+
+-- use bullet notations to structure the proof in a hierarchical manner
+theorem test6 (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  . exact hp
+  . apply And.intro
+    . exact hq
+    . exact hp
+
+---------------------
+--- Basic tactics ---
+---------------------
+
+-- We have seen `apply`, `exact`. There is also `intro`.
+
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  . intro h
+    apply Or.elim (And.right h)
+    . intro boo
+      apply Or.inl
+      apply And.intro
+      . exact And.left h
+      . exact boo
+    . intro boo
+      apply Or.inr
+      apply And.intro
+      . exact And.left h
+      . exact boo
+  . intro h
+    apply And.intro
+    . apply (Or.elim h)
+      . intro t
+        exact (And.left t)
+      . intro t
+        exact (And.left t)
+    . apply (Or.elim h)
+      . intro boo
+        apply Or.inl
+        exact (And.right boo)
+      . intro boo
+        apply Or.inr
+        exact (And.right boo)
+
+
+-- `apply` tactic constructs function applications interactively
+-- `intro` tactic constructs functions abstractions interactively
+
+example (α : Type) (p q : α → Prop) : (∃ x, p x ∧ q x) → ∃ x, q x ∧ p x := by
+  intro ⟨w, hpw, hqw⟩
+  exact ⟨w, hqw, hpw⟩
+
+def ex13 (α : Type) (p q : α → Prop) : (∃ x, p x ∨ q x) → ∃ x, q x ∨ p x := by
+  intro
+  | ⟨w, Or.inl h⟩ => exact ⟨w, Or.inr h⟩
+  | ⟨w, Or.inr h⟩ => exact ⟨w, Or.inl h⟩
+
+#print ex13
+
+-- The `assumption` tactic
+
+example (x y z w : Nat) (h₁ : x = y) (h₂ : y = z) (h₃ : z = w) : x = w := by
+  apply Eq.trans h₁
+  apply Eq.trans h₂
+  assumption   -- applied h₃
+
+example (x y z w : Nat) (h₁ : x = y) (h₂ : y = z) (h₃ : z = w) : x = w := by
+  apply Eq.trans
+  . assumption      -- solves x = ?b with h₁
+  . apply Eq.trans
+    . assumption      -- solves y = ?h₂.b with h₂
+    . assumption      -- solves z = w with h₃
+
+-- variables introduced by `intros` cannot be used manually. To disable this, use `unhygienic`
+example : ∀ a b c : Nat, a = b → a = c → c = b := by unhygienic
+  intros
+  apply Eq.trans
+  apply Eq.symm
+  exact a_2
+  exact a_1
+
+-- Another tactic `rename_i h1 _ h2`
+
+-- `revert` is the inverse of `intro`
+
+example (x : Nat) : x = x := by
+  revert x
+  -- goal is ⊢ ∀ (x : Nat), x = x
+  intro y
+  -- goal is y : Nat ⊢ y = y
+  rfl
+
+-- `revert` brings the dependencies with it
+example (x y : Nat) (h : x = y) : y = x := by
+  revert x
+  -- goal is y : Nat ⊢ ∀ (x : Nat), x = y → y = x
+  intros
+  apply Eq.symm
+  assumption
+
+-- `generalize`
+
+example : 3 = 3 := by
+  generalize 3 = x
+  -- goal is x : Nat ⊢ x = x
+  revert x
+  -- goal is ⊢ ∀ (x : Nat), x = x
+  intro y
+  -- goal is y : Nat ⊢ y = y
+  rfl
+
+example : 2 + 3 = 5 := by
+  generalize h : 3 = x
+  -- goal is x : Nat, h : 3 = x ⊢ 2 + x = 5
+  rw [← h]
+
+
+--------------------
+--- More Tactics ---
+--------------------
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h with
+  | inr hq => apply Or.inl; exact hq
+  | inl hp => apply Or.inr; exact hp
+
+example (p q : Prop) : p ∨ q → q ∨ p := by
+  intro h
+  cases h
+  apply Or.inr
+  assumption
+  apply Or.inl
+  assumption
+
+
+-- using `show` tells the type of the goal that is being proven
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  . intro h
+    cases h.right with
+    | inl hq =>
+      show (p ∧ q) ∨ (p ∧ r)
+      exact Or.inl ⟨h.left, hq⟩
+    | inr hr =>
+      show (p ∧ q) ∨ (p ∧ r)
+      exact Or.inr ⟨h.left, hr⟩
+  . intro h
+    cases h with
+    | inl hpq =>
+      show p ∧ (q ∨ r)
+      exact ⟨hpq.left, Or.inl hpq.right⟩
+    | inr hpr =>
+      show p ∧ (q ∨ r)
+      exact ⟨hpr.left, Or.inr hpr.right⟩
+
+-- `show` is for exactly the goal or something equivalent
+example (n : Nat) : n + 1 = Nat.succ n := by
+  show Nat.succ n = Nat.succ n
+  rfl
+
+-- Tactic `have` to introduce a new subgoal
+example (p q r : Prop) : p ∧ (q ∨ r) → (p ∧ q) ∨ (p ∧ r) := by
+  intro ⟨hp, hqr⟩
+  show (p ∧ q) ∨ (p ∧ r)
+  cases hqr with
+  | inl hq =>
+    have hpq : p ∧ q :=
+      And.intro hp hq
+    apply Or.inl
+    exact hpq
+  | inr hr =>
+    have hpr : p ∧ r := And.intro hp hr
+    apply Or.inr
+    exact hpr
+
+-- It is possible to omit the term and type with `have`
+example (p q r : Prop) : p ∧ (q ∨ r) → (p ∧ q) ∨ (p ∧ r) := by
+  intro ⟨hp, hqr⟩
+  cases hqr with
+  | inl hq =>
+    have := And.intro hp hq
+    apply Or.inl; exact this
+  | inr hr =>
+    have := And.intro hp hr
+    apply Or.inr; exact this
+
+
+-- Tactic `let` to introduce terms
+example : ∃ x, x + 2 = 8 := by
+  let a : Nat := 3 * 2
+  exists a
+
+
+-- We have used bullets `.` but it is also possible to use braces
+example (p q r : Prop) : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  apply Iff.intro
+  { intro h;
+    cases h.right;
+    { show (p ∧ q) ∨ (p ∧ r);
+      exact Or.inl ⟨h.left, ‹q›⟩ }
+    { show (p ∧ q) ∨ (p ∧ r);
+      exact Or.inr ⟨h.left, ‹r›⟩ } }
+  { intro h;
+    cases h;
+    { show p ∧ (q ∨ r);
+      rename_i hpq;
+      exact ⟨hpq.left, Or.inl hpq.right⟩ }
+    { show p ∧ (q ∨ r);
+      rename_i hpr;
+      exact ⟨hpr.left, Or.inr hpr.right⟩ } }
+
+
+--------------------------
+--- Tactic Combinators ---
+--------------------------
+
+-- Tactic combinators are operations that form new tactics from old ones.
+
+-- `;` and `<;>`, the former sequences tactics, the latter provides a parallel version of sequencing.
+
+example (p q : Prop) (hp : p) : p ∨ q :=
+  by apply Or.inl; assumption
+
+example (p q : Prop) (hp : p) (hq : q) : p ∧ q :=
+  by constructor <;> assumption
+
+-- `first | t1 | t2 | ... | tn` tries each `ti` until one succeeds.
+
+-- In the following example the same combined tactics works in both cases
+example (p q : Prop) (hp : p) : p ∨ q := by
+  first | apply Or.inl; assumption | apply Or.inr; assumption
+
+example (p q : Prop) (hq : q) : p ∨ q := by
+  first | apply Or.inl; assumption | apply Or.inr; assumption
+
+-- And this can be repeated
+example (p q r : Prop) (hr : r) : p ∨ q ∨ r :=
+  -- by repeat (first | apply Or.inl ; assumption | apply Or.inr | assumption)
+  by apply Or.inr
+     apply Or.inr
+     assumption
+
+example (p q r : Prop) (hq : q) : p ∨ q ∨ r :=
+  -- by repeat (first | apply Or.inl ; assumption | apply Or.inr | assumption)
+  by apply Or.inr
+     apply Or.inl
+     assumption
+
+example (p q r : Prop) (hp : p) : p ∨ q ∨ r :=
+  -- by repeat (first | apply Or.inl ; assumption | apply Or.inr | assumption)
+  by apply Or.inl
+     assumption
+
+-- `try t` is equivalent to `first | t | skip`
+example (p q r : Prop) (hp : p) (hq : q) (hr : r) : p ∧ q ∧ r := by
+  constructor <;> (try constructor) <;> assumption
+
+-- The tactic `all_goals` is another way to use parallel sequencing and solve all the branches with the same tactic
+example (p q r : Prop) (hp : p) (hq : q) (hr : r) : p ∧ q ∧ r := by
+  constructor
+  all_goals (try constructor)
+  all_goals assumption
+
+-- The tactic `any_goals` is like `all_goals` but more robust, succeeding only if at least one goal succeeds
+example (p q r : Prop) (hp : p) (hq : q) (hr : r) : p ∧ q ∧ r := by
+  constructor
+  any_goals constructor
+  any_goals assumption
+
+-- point the cursor after `repeat` and see the sequence
+example (p q r : Prop) (hp : p) (hq : q) (hr : r) :
+      p ∧ ((p ∧ q) ∧ r) ∧ (q ∧ r ∧ p) := by
+  repeat (any_goals constructor)
+  all_goals assumption
+
+-- The tactic combinator `focus t` applies the tactic `t` only on the first goal.
+
+-----------------
+--- Rewriting ---
+-----------------
+
+
+-- Tactic `rewrite [t]` or `rw [t]`
+example (f : Nat → Nat) (k : Nat) (h₁ : f 0 = 0) (h₂ : k = 0) : f k = 0 := by
+  rw [h₂] -- replace k with 0
+  rw [h₁] -- replace f 0 with 0
+
+example (x y : Nat) (p : Nat → Prop) (q : Prop) (h : q → x = y)
+        (h' : p y) (hq : q) : p x := by
+  rw [h hq]; assumption
+
+-- chaining rewrites
+example (f : Nat → Nat) (k : Nat) (h₁ : f 0 = 0) (h₂ : k = 0) : f k = 0 := by
+  rw [h₂, h₁]
+
+-- Rewriting in the other direction with `rw [←t]`
+example (f : Nat → Nat) (a b : Nat) (h₁ : a = b) (h₂ : f a = 0) : f b = 0 := by
+  rw [←h₁, h₂]
+
+-- Specifying the term to rewrite more precisely
+example (a b c : Nat) : a + b + c = a + c + b := by
+  rw [Nat.add_assoc, Nat.add_comm b, ← Nat.add_assoc]
+
+example (a b c : Nat) : a + b + c = a + c + b := by
+  rw [Nat.add_assoc, Nat.add_assoc, Nat.add_comm b]
+
+example (a b c : Nat) : a + b + c = a + c + b := by
+  rw [Nat.add_assoc, Nat.add_assoc, Nat.add_comm _ b]
+
+-- rewriting tactics in hypotheses with `rw[t] at h`
+example (f : Nat → Nat) (a : Nat) (h : a + 0 = 0) : f a = f 0 := by
+  rw [Nat.add_zero] at h
+  rw [h]
+
+
+------------------
+--- Simplifier ---
+------------------
+
+example (x y z : Nat) : (x + 0) * (0 + y * 1 + z * 0) = x * y := by
+  simp
+
+example (x y z : Nat) (p : Nat → Prop) (h : p (x * y))
+        : p ((x + 0) * (0 + y * 1 + z * 0)) := by
+  simp; assumption
+
+-- Tactic `simpl at h`
+example (x y z : Nat) (p : Nat → Prop)
+        (h : p ((x + 0) * (0 + y * 1 + z * 0))) : p (x * y) := by
+  simp at h; assumption
+
+
+-- Tactic `simple at *` to simplify in all hypotheses aith "ordered rewriting"
+attribute [local simp] Nat.mul_comm Nat.mul_assoc Nat.mul_left_comm
+attribute [local simp] Nat.add_assoc Nat.add_comm Nat.add_left_comm
+example (x y z : Nat) (p : Nat → Prop)
+        (h₁ : p (1 * x + y)) (h₂ : p (x * z * 1))
+        : p (y + 0 + x) ∧ p (z * x) :=
+  by
+    simp at * ; constructor <;> assumption
+
+example (w x y z : Nat) (p : Nat → Prop)
+        : x * y + z * w * x = x * w * z + y * x := by
+  simp
+
+example (w x y z : Nat) (p : Nat → Prop)
+        (h : p (x * y + z * w * x)) : p (x * w * z + y * x) := by
+  simp; simp at h; assumption
+
+
+-- Can we guess what is this "ordered rewriting" and the associated canonical order.
+
+
+-- Like `rw`, `simpl [t1, <-t2]`
+def f (m n : Nat) : Nat :=
+  m + n + m
+
+example {m n : Nat} (h : n = 1) (h' : 0 = m) : (f m n) = n := by
+  simp [h, ←h', f]
+
+
+def mk_symm (xs : List α) :=
+  xs ++ xs.reverse
+
+theorem reverse_mk_symm (xs : List α)
+        : (mk_symm xs).reverse = mk_symm xs := by
+  unfold mk_symm
+  simp
+  -- or directly simp [mk_symm]
+
+-- can make it a simplification rule directly using a decorator `@[simpl]`
+@[simp] theorem reverse_mk_symm2 (xs : List α)
+        : (mk_symm xs).reverse = mk_symm xs := by
+  simp [mk_symm]
+
+example (xs ys : List Nat)
+        : (xs ++ mk_symm ys).reverse = mk_symm ys ++ xs.reverse := by
+  simp
+
+example (xs ys : List Nat) (p : List Nat → Prop)
+        (h : p (xs ++ mk_symm ys).reverse)
+        : p (mk_symm ys ++ xs.reverse) := by
+  simp at h; assumption
+
+-- there are more subtleties/feature to register theorems as simplification rules and limit
+-- their scope
+
+
+-------------
+--- Split ---
+-------------
+
+def f1 (x y z : Nat) : Nat :=
+  match x, y, z with
+  | 5, _, _ => y
+  | _, 5, _ => y
+  | _, _, 5 => y
+  | _, _, _ => 1
+
+example (x y z : Nat) : x ≠ 5 → y ≠ 5 → z ≠ 5 → z = w → f1 x y w = 1 := by
+  intros
+  simp [f1]
+  split
+  . contradiction
+  . contradiction
+  . contradiction
+  . rfl
+
+-- Factor this proof...
+
+
+def g (xs ys : List Nat) : Nat :=
+  match xs, ys with
+  | [a, b], _ => a+b+1
+  | _, [b, c] => b+1
+  | _, _      => 1
+
+example (xs ys : List Nat) (h : g xs ys = 0) : False := by
+  simp [g] at h; split at h <;> simp_arith at h
+
+
+
+
+
+----------------
+--- Exercise ---
+----------------
+
+variable (p q r : Prop)
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := sorry
+example : p ∨ q ↔ q ∨ p := sorry
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+example : ¬(p ∧ ¬p) := sorry
+example : p ∧ ¬q → ¬(p → q) := sorry
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ False ↔ p := sorry
+example : p ∧ False ↔ False := sorry
+example : (p → q) → (¬q → ¬p) := sorry
