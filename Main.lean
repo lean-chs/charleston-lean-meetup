0a6a819c6634c4d0b8258f3f8585a3023c1dafd5
Jared Corduan <jaredcorduan@gmail.com>
2024-12-10 09:45:36 -0500

    notes for 2024-12-10

--- Main.lean
+++ Main.lean
@@ -1,8 +1,37 @@
-import «CharlestonLeanMeetup»
-open notes20240813
+import Std
+open Std (HashMap)
 
-def main (args : List String) : IO UInt32 := do
-  IO.println s!"list:   {args}"
-  IO.println s!"vector: {list_to_vector args}"
-  IO.println s!"array: {Array.mk args}"
-  pure 0
+#check compare
+#check HashMap
+#check HashMap.alter
+#check HashMap.empty
+#check List.drop
+#check List.filterMap
+#check List.flatten
+#check List.foldl
+#check List.map
+#check List.mergeSort
+#check List.range
+#check List.take
+#check Option.none
+#check Option.some
+#check Ordering
+#check (Ordering.eq, Ordering.lt, Ordering.gt)
+#check String.mk
+#check String.splitOn
+#check String.toList
+#check String.toNat!
+
+
+def day_1_a (lines : List String) : Nat := sorry
+
+
+def main (args : List String) : IO UInt32 :=
+  match args with
+  | [] => do
+      IO.println "pass a filename"
+      pure 1
+  | filename::_ => do
+      let ans <- (day_1_a ∘ Array.toList) <$> IO.FS.lines ⟨filename⟩
+      IO.println ans
+      pure 0
